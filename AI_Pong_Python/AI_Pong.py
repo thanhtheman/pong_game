0f46bd75fd985e6288f0fc54e2a7c82031e88d3e
thanh quach <thanhb.quach@gmail.com>
2022-09-14 19:06:20 -0400

    set up AI

--- AI_Pong_Python/AI_Pong.py
+++ AI_Pong_Python/AI_Pong.py
@@ -5,18 +5,31 @@ from pong.game import Game
 width, height = 700, 500
 window = pygame.display.set_mode((width, height))
 
-game = Game(window, width, height)
+class PongGame:
+    def __init__(self, window, width, height):
+        self.game = Game(window, width, height)
+        self.left_paddle = self.game.left_paddle
+        self.right_paddle = self.game.right_paddle
+        self.ball = self.game.ball
+    
+    def test_ai():
+        run = True
+        clock = pygame.time.Clock()
 
-run = True
-clock = pygame.time.Clock()
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    break
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                game.move_paddle(left=True, up=True)
+            if keys[pygame.K_s]:
+                game.move_paddle(left=True, up=False)
+            
+            game.loop()
+            game.draw(False, True)
+            pygame.display.update()
+        pygame.quit()
 
-while run:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            break
-    game.loop()
-    game.draw()
-    pygame.display.update()
-pygame.quit()
